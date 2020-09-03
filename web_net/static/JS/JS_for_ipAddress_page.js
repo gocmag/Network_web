360c4ccd59651bd3acf7adca1eb33d9bb2303714
root <gocmag@gmail.com>
2020-09-03 13:46:12 +0300

    Добавлено: - функция чеклистов в таблице геокодов

--- web_net/static/JS/JS_for_ipAddress_page.js
+++ web_net/static/JS/JS_for_ipAddress_page.js
@@ -6,16 +6,10 @@ let x = $('td *').on('click',function () {
     let id = this.id;
     let native_top = $(this).offset().top;
     let native_left = $(this).offset().left;
-  //  console.log($(this).offset());
 
-   // $('#changeDescription').offset({top: native_top+10, left: native_left+120}).fadeIn()
-   // console.log($('#changeDescription').offset());
-   // $('#changeDescription2').show()
     let digits = parseInt(id.match(/\d+/));
     let newChangeDescriptionId = '#'+'changeDescription'+digits;
     $(newChangeDescriptionId).fadeIn(800);
-    console.log(digits)
-
 });
 
 
--- web_net/static/JS/JS_for_region_page.js
+++ web_net/static/JS/JS_for_region_page.js
@@ -3,3 +3,20 @@ $('#regionForm').hide()
 $('#addRegion').on('click',function () {
     $('#regionForm').slideToggle(1500);
 });
+
+$('.geokodObject tr').on('click', function () {
+    $(this).find('td').css({
+        background: "rgb(201, 93, 16)",
+        transition: "0.7s"
+    });
+    let elem = document.querySelector('.geokodObject td');
+    let test = $(this).find('td').css('background');
+    let test2 = $(event.target).css('background-color');
+    console.log(test2);
+    if (test2 === 'rgb(201, 93, 16)') {
+        $(this).find('td').css({
+        background: "rgb(248, 248, 255)",
+        transition: "0.7s"})
+    }
+});
+
