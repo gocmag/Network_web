f0f15b1d7a093fb143f74d539fdb9548e0d62af3
root <gocmag@gmail.com>
2020-09-25 17:52:19 +0300

    Добавлена система авторизации

--- web_net/views.py
+++ web_net/views.py
@@ -5,9 +5,11 @@ from .models import Region, Networks, VLAN, Adress, PAT
 from .forms import NetworkForm, RegionForm, VlanForm, ipaddressForm, changeNetwork, changeLocationNetwork, changeVlan
 from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/accounts/login/')
 def choise_page(request, region_id):
     return render(request, 'choise_page.html',{'region_id':region_id})
 
+@login_required(login_url='/accounts/login/')
 def networking(request, region_id):
     networks = Networks.objects.all().order_by('network')
     networks_for_region = Networks.objects.filter(region_reletionship=region_id)
@@ -36,6 +38,7 @@ def networking(request, region_id):
 
     return render(request, 'Network_page.html', parametrs)
 
+@login_required(login_url='/accounts/login/')
 def vlans(request, region_id):
     vlans_for_region = VLAN.objects.filter(region_reletionship=region_id)
     region_object = Region.objects.get(id=region_id)
@@ -79,6 +82,7 @@ def region(request):
             form.save()
     return render(request, 'region_page.html', parametrs)
 
+@login_required(login_url='/accounts/login/')
 def address(request, region_id, network_id):
     network_object = Networks.objects.get(id=network_id)
     current_network_objects = Networks.objects.filter(id=network_id)
@@ -138,10 +142,12 @@ def address(request, region_id, network_id):
 
     return render(request, 'ip_address_page.html', parametrs)
 
+@login_required(login_url='/accounts/login/')
 def from_vlan_to_address (request,region_id,vlan_id):
     network_object = Networks.objects.get(region_reletionship=region_id, vlan_reletionship=vlan_id)
     return redirect('address', region_id=region_id, network_id=network_object.id)
 
+@login_required(login_url='/accounts/login/')
 def search_view(request):
     q = request.GET.get('q', None)
     searchNetworkResult = Networks.objects.filter(
